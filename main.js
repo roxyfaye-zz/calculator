cd934c6b45877d7d79cb0538d48edca8dc9e2057
roxyfaye <rdesantisnunally@gmail.com>
2017-06-18 16:05:13 -0400

    submit

--- main.js
+++ main.js
@@ -1,41 +1,62 @@
 var display = document.getElementById('answer');
 
-//var operatorArrays = document.getElementsByClassName('math');
+var displayString = "";
 
-//operatorArray[0].value = C;
-//operatorArray[1].value = "/";
-//operatorArray[2].value = X;
-//operatorArray[3].value = "-";
-//operatorArray[4].value = "*";
-//operatorArray[5].value = "-";
-//operatorArray[6].value = ".";
-//operatorArray[7].value = "=";
-//operatorArray[8].value = "+";
+var clearButton = document.getElementById('clear');
+
+var operatorsArray = document.getElementsByClassName('operator');
 
 var numbersArray = document.getElementsByClassName('number');
 
-numbersArray[0].value = 7;
-numbersArray[1].value = 8;
-numbersArray[2].value = 9;
-numbersArray[3].value = 4;
-numbersArray[4].value = 5;
-numbersArray[5].value = 6;
-numbersArray[6].value = 1;
-numbersArray[7].value = 2;
-numbersArray[8].value = 3;
-numbersArray[9].value = 0;
+function updateDisplayScreen(newChar) {
 
+    displayString += newChar;
 
-for(let i = 0; i < numbersArray.length; i++) {
+    display.innerHTML = displayString;
 
-    numbersArray[i].addEventListener("click", function(){
-  
-    display.innerHTML=numbersArray[i].value;
-
-    console.log(numbersArray[i].value);
-
-});
 }
 
+function handleClearPress(event) {
+
+    displayString = "";
+
+    display.innerHTML = displayString;
+
+}
+
+function handleNumberPress(event) {
+
+    var numberThatWasPressed = event.target;
+
+    updateDisplayScreen(numberThatWasPressed.innerHTML);
+
+}
+
+function handleOperatorPress(event) {
+
+    var operatorPress = event.target;
+
+    updateDisplayScreen(operatorPress.innerHTML);
+
+}
+
+for (let i = 0; i < numbersArray.length; i++) {
+
+    numbersArray[i].addEventListener("click", handleNumberPress);
+}
+
+for (let i = 0; i < operatorsArray.length; i++) {
+
+    operatorsArray[i].addEventListener("click",handleOperatorPress);
+
+}
+
+clearButton.addEventListener("click", handleClearPress);
 
 
+
+
+
+
+///push values into the string when the "click" function is run (concatonate?) then 
+// eval() that string.
